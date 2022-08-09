0295138b4dfc6d56d2c02eb723a75b9fbb075e57
truongnda <sdtla0911114819@gmail.com>
2022-08-09 14:47:48 +0700

    productcount

--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def store(request):
+    products = Product.objects.all().filter(is_available=True)
+    product_count = products.count()
+    
+    # Truyen bien sang
+    context = {
+        'products': products,
+        'product_count': product_count,
+        
+    }
+    return render(request, 'store/store.html', context)
--- truongndacart/views.py
+++ truongndacart/views.py
@@ -3,12 +3,9 @@ from store.models import Product
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
-    print(products)
-    # products = Product.objects.all().filter(is_available=True)
-    # print(products)
     # Truyen bien sang
     context = {
-        'products': products
+        'products': products,
     }
     return render(request, 'home.html', context)
 
