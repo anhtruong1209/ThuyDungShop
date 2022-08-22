2a9219e62efd4087e50cc99ff11fb3e1e49e4531
Quynhtram1220 <doquynhtram2010@gmail.com>
2022-08-22 22:17:48 +0700

    22082022

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from category.models import Category
-
+from django.urls import reverse
 
 # Create your models here.
 
@@ -16,5 +16,8 @@ class Product(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
 
+    def get_url(self):
+        return reverse('product_detail', args=[self.category.slug, self.slug])
+    
     def __str__(self):
         return self.product_name
