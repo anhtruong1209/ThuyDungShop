a9b7ce7e5c5a5609666173da9909187a01840f3a
anhtruong1209 <sdtla0911114819@gmail.com>
2022-08-25 21:03:24 +0700

    dsad

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from http.client import HTTPResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
@@ -34,6 +34,17 @@ def add_cart(request, product_id):
         cart_item.save()
     return redirect('cart')
 
+def remove_cart(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product, id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+        
+    return redirect('cart')
 def cart(request, total=0, quantity=0, cart_items=None):
     try:
         tax = 0
